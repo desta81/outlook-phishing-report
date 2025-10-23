4438fe4e883759f16b38b682c23f9a3c4411c853
samuel zewde <samuelzewde29@gmail.com>
2025-10-23 11:50:14 +0300

    working build

---
+++ OutlookSpamReporter/SpamReporterRibbon.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.IO;
+using OutlookSpamReporter.Utilities;
+
+namespace OutlookSpamReporter
+{
+    public partial class SpamReporterRibbon
+    {
+        private void SpamReporterRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnReportSpam_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                var application = Globals.ThisAddIn.Application;
+                FileLogger.Info("Report Spam clicked");
+                Outlook.Selection selection = application.ActiveExplorer().Selection;
+                if (selection == null || selection.Count == 0)
+                {
+                    FileLogger.Info("No items selected");
+                    System.Windows.Forms.MessageBox.Show("Please select at least one email.");
+                    return;
+                }
+
+
+                Outlook.MailItem forwardMail = application.CreateItem(Outlook.OlItemType.olMailItem) as Outlook.MailItem;
+                forwardMail.Subject = "Spam Report";
+                forwardMail.To = "samuelzewde29@gmail.com";
+                forwardMail.Body = "Please review the attached spam emails for investigation.";
+
+                for (int i = 1; i <= selection.Count; i++)
+                {
+                    object item = selection[i];
+                    var mail = item as Outlook.MailItem;
+                    if (mail == null)
+                    {
+                        FileLogger.Info("Skipped non-mail selection item");
+                        continue;
+                    }
+
+                    string tempPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N") + ".msg");
+                    mail.SaveAs(tempPath, Outlook.OlSaveAsType.olMSGUnicode);
+                    forwardMail.Attachments.Add(tempPath, Outlook.OlAttachmentType.olByValue, Type.Missing, mail.Subject);
+                    FileLogger.Info("Attached message: " + tempPath);
+                }
+
+                forwardMail.Send();
+                FileLogger.Info("Spam report email sent automatically");
+            }
+            catch (Exception ex)
+            {
+                FileLogger.Error("Error creating spam report", ex);
+            }
+        }
+
+        private void btnReportSpamRead_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                var application = Globals.ThisAddIn.Application;
+                FileLogger.Info("Report Spam (Read) clicked");
+
+                // Get the currently open mail item
+                Outlook.Inspector inspector = application.ActiveInspector();
+                if (inspector == null)
+                {
+                    FileLogger.Info("No mail item open");
+                    System.Windows.Forms.MessageBox.Show("Please open an email to report as spam.");
+                    return;
+                }
+
+                Outlook.MailItem currentMail = inspector.CurrentItem as Outlook.MailItem;
+                if (currentMail == null)
+                {
+                    FileLogger.Info("Current item is not a mail item");
+                    System.Windows.Forms.MessageBox.Show("Please open an email to report as spam.");
+                    return;
+                }
+
+                Outlook.MailItem forwardMail = application.CreateItem(Outlook.OlItemType.olMailItem) as Outlook.MailItem;
+                forwardMail.Subject = "Spam Report";
+                forwardMail.To = "samuelzewde29@gmail.com";
+                forwardMail.Body = "Please review the attached spam email for investigation.";
+
+                string tempPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N") + ".msg");
+                currentMail.SaveAs(tempPath, Outlook.OlSaveAsType.olMSGUnicode);
+                forwardMail.Attachments.Add(tempPath, Outlook.OlAttachmentType.olByValue, Type.Missing, currentMail.Subject);
+                FileLogger.Info("Attached message: " + tempPath);
+
+                forwardMail.Send();
+                FileLogger.Info("Spam report email sent automatically");
+            }
+            catch (Exception ex)
+            {
+                FileLogger.Error("Error creating spam report", ex);
+            }
+        }
+    }
+}
